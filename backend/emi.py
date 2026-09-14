5ca8d4eb2959028c7affae2ceb2bcd00f0dce266
Vivek Parashar <vivekparashar47204@gmail.com>
2026-09-15 02:17:24 +0530

    the core feature is added, which is the toggle bar for amount change

--- backend/emi.py
+++ backend/emi.py
@@ -22,22 +22,41 @@ class user_detail(BaseModel):
     BikeCompany: str
 
 
-@app.post("/check-emi")
-def emi_checker(emi_details: user_detail):
-    if emi_details.MonthlySalary >= 100000:
-        return {
+# @app.post("/check-emi")
+# def emi_checker(emi_details: user_detail):
+#     if emi_details.MonthlySalary >= 100000:
+#         return {
 
-            "name": emi_details.Fullname,
-            "decision": f"congratulations!!! {emi_details.Fullname}, you can comfortably afford this bike"
-        }
-    else:
-        return{
-            "name": emi_details.Fullname,
-            "decision": f"Sorry {emi_details.Fullname}, you are not eligible right now"
-        }
+#             "name": emi_details.Fullname,
+#             "decision": f"congratulations!!! {emi_details.Fullname}, you can comfortably afford this bike"
+#         }
+#     else:
+#         return{
+#             "name": emi_details.Fullname,
+#             "decision": f"Sorry {emi_details.Fullname}, you are not eligible right now"
+#         }
     
 
 
+
+@app.post("/check-emi")
+def emi_checker(emi_details: user_detail):
+
+    if emi_details.MonthlySalary >= 100000:
+
+        return {
+            "name": emi_details.Fullname,
+            "eligible": True,
+            "decision": f"Congratulations {emi_details.Fullname}, you can comfortably afford this bike"
+        }
+
+    else:
+
+        return {
+            "name": emi_details.Fullname,
+            "eligible": False,
+            "decision": f"Sorry {emi_details.Fullname}, you are not eligible right now"
+        }
     
 
     
