ca8abf6b37fe2c5e7f4795712948c13cf1628667
Vivek Parashar <vivekparashar47204@gmail.com>
2026-09-15 02:51:41 +0530

    changes done on backend

--- frontend/intro_page.js
+++ frontend/intro_page.js
@@ -1,5 +1,5 @@
 
-const API_URL = "http://127.0.0.1:8000/check-emi";
+const API_URL = "https://superbike-price-predictor-3.onrender.com/check-emi";
 
 const form = document.getElementById("applicant-form");
 const errorEl = document.getElementById("form-error");
